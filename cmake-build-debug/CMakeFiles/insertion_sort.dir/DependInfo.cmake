4a7748d2bfdf2747f9e42695e39db698426ecd18
futur <futureof723@gmail.com>
2025-02-05 00:32:53 -0500

    Created .gitignore

---
+++ cmake-build-debug/CMakeFiles/insertion_sort.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/futur/CLionProjects/sorting_algos/main.cpp" "CMakeFiles/insertion_sort.dir/main.cpp.obj" "gcc" "CMakeFiles/insertion_sort.dir/main.cpp.obj.d"
+  "C:/Users/futur/CLionProjects/sorting_algos/sorting.cpp" "CMakeFiles/insertion_sort.dir/sorting.cpp.obj" "gcc" "CMakeFiles/insertion_sort.dir/sorting.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/insertion_sort.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/insertion_sort.dir/main.cpp.obj"
+  "CMakeFiles/insertion_sort.dir/main.cpp.obj.d"
+  "CMakeFiles/insertion_sort.dir/sorting.cpp.obj"
+  "CMakeFiles/insertion_sort.dir/sorting.cpp.obj.d"
+  "insertion_sort.exe"
+  "insertion_sort.exe.manifest"
+  "insertion_sort.pdb"
+  "libinsertion_sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/insertion_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
